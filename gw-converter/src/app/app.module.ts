adb04cc118e617c83d47617433d3d6449e412ddd
chrisgustafson-okta <53533241+chrisgustafson-okta@users.noreply.github.com>
2022-01-19 11:34:53 -0600

    Added Auth

--- gw-converter/src/app/app.module.ts
+++ gw-converter/src/app/app.module.ts
@@ -6,15 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthModule } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MainNavComponent
+    MainNavComponent,
+    LoginButtonComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
+    AuthModule.forRoot({
+      domain: 'gusww.us.auth0.com',
+      clientId: 'hShD0UJALOnrtnZ54w6I4UN4zrgrBdGg'
+    }),
     AppRoutingModule,
     NgbModule
   ],
---
+++ gw-converter/src/app/login-button/login-button.component.ts
@@ -0,0 +1,27 @@
+import { Component,  Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-login-button',
+  template: `
+    <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
+      <button (click)="auth.logout({ returnTo: document.location.origin })">
+        Log out
+      </button>
+    </ng-container>
+
+    <ng-template #loggedOut>
+      <button (click)="auth.loginWithRedirect()">Log in</button>
+    </ng-template>
+  `,
+  styleUrls: ['./login-button.component.sass']
+})
+export class LoginButtonComponent {
+
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+
+  ngOnInit(): void {
+  }
+
+}
